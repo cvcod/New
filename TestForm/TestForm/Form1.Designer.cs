af4bfa839fa330b40f04fc7d50051b8e907ad6aa
cvcod <35714533+cvcod@users.noreply.github.com>
2018-03-20 15:28:37 -0400

    Add files via upload

---
+++ TestForm/TestForm/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace TestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisp = new System.Windows.Forms.Button();
+            this.Display = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDisp
+            // 
+            this.btnDisp.Location = new System.Drawing.Point(99, 174);
+            this.btnDisp.Name = "btnDisp";
+            this.btnDisp.Size = new System.Drawing.Size(75, 23);
+            this.btnDisp.TabIndex = 0;
+            this.btnDisp.Text = "button1";
+            this.btnDisp.UseVisualStyleBackColor = true;
+            this.btnDisp.Click += new System.EventHandler(this.btnDisp_Click);
+            // 
+            // Display
+            // 
+            this.Display.Location = new System.Drawing.Point(77, 81);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(125, 20);
+            this.Display.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.btnDisp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisp;
+        private System.Windows.Forms.TextBox Display;
+    }
+}
+
